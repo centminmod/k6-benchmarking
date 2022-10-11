63a0e08e7f7c018b2863270b2f1d76e326a161ba
George Liu (eva2000) <eva2000@centminmod.com>
2022-10-11 22:20:41 +1000

    add k6-log-to-influxdb.sh converter
    - allows converting k6 JSON output log to InfluxDB batch line write format

---
+++ tools/k6-log-to-influxdb.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+###############################################################################
+# take k6 run with --out json=summary-raw-scenarios-multi.gz JSON logged output
+# and convert it into InfluxDB 1.8 batch line write format for post-k6 run
+# insertion into InfluxDB 1.8 database
+# https://github.com/centminmod/k6-benchmarking/blob/master/logs/k6-log-parsing.md
+###############################################################################
+# break to 5000 size batches for InfluxDB
+SPLITSIZE='5000'
+INFLUXDB_NAME='k6'
+INFLUXDB_PORT='8186'
+INFLUXDB_HOST='localhost'
+
+WORKDIR="/home/k6-workdir"
+LOGS_VU="${WORKDIR}/filtered-vus.log"
+LOGS_METRICS="${WORKDIR}/filtered-metrics.log"
+INFLUX_FORMAT_VU="${WORKDIR}/influxdb-vus.log"
+INFLUX_FORMAT_METRICS="${WORKDIR}/influxdb-metrics.log"
+###############################################################################
+if [ ! -d "$WORKDIR" ]; then
+  mkdir -p "$WORKDIR"
+fi
+if [ ! -f /usr/bin/jq ]; then
+  yum -y -q install jq
+fi
+if [ -f /usr/bin/pzcat ]; then
+  ZCATBIN=/usr/bin/pzcat
+else
+  ZCATBIN=/usr/bin/zcat
+fi
+
+convert_to_influx() {
+  cd "$WORKDIR"
+  mode="$1"
+  file="$2"
+  if [[ "$mode" = 'influx' ]]; then
+    # grab vus metric
+    $ZCATBIN "$file" | jq -c '. | select(.type=="Point" and .metric == "vus")' > "$LOGS_VU"
+    # grab the rest of the metrics
+    $ZCATBIN "$file" | jq -c '. | select(.type=="Point" and .data.tags.expected_response == "true" and .data.tags.status >= "200")' > "$LOGS_METRICS"
+    # parse into InfluxDB batch write formats
+    # vus metrics
+    cat "$LOGS_VU" | jq -r '"\(.metric),\(.data.tags|to_entries|map("\(.key)=\(.value|tostring)")|[.[]]|sort|join(",")) value=\(.data.value) \((.data.time[0:19] + "Z"|fromdateiso8601)*1000000000 + (.data.time[20:24-27]|tonumber))"' > "$INFLUX_FORMAT_VU"
+    # rest of metrics
+    cat "$LOGS_METRICS" | jq -r '"\(.metric),\(.data.tags|to_entries|map("\(.key)=\(.value|tostring)")|[.[]]|sort|join(",")) value=\(.data.value) \((.data.time[0:19] + "Z"|fromdateiso8601)*1000000000 + (.data.time[20:24-27]|tonumber))"' > "$INFLUX_FORMAT_METRICS"
+    # count lines
+    _array=("$INFLUX_FORMAT_VU" "$INFLUX_FORMAT_METRICS")
+
+    for c in "${_array[@]}"; do
+      fdirname=$(dirname "$c")
+      fbasename=$(basename "$c")
+      counter=$(cat "$c"|wc -l)
+      if [[ "$counter" -gt "$SPLITSIZE" ]]; then
+        # remove previous files
+        find $fdirname -type f -name "${fbasename}-split-*" -delete
+        split -l "$SPLITSIZE" $c ${fbasename}-split-
+        echo
+        echo "     Saved & Split InfluxDB formatted data files ($counter lines) at:"
+        find $fdirname -type f -name "${fbasename}-split-*" | while read f; do
+          fn=$(basename $f)
+          count_file=$(cat $fn|wc -l)
+          echo "     ${WORKDIR}/$fn ($count_file)"
+        done
+        echo
+        echo "     InfluxDB import queries"
+        echo
+        echo "     curl -i -sX POST http://${INFLUXDB_HOST}:${INFLUXDB_PORT}/query --data-urlencode \"q=CREATE DATABASE "$INFLUXDB_NAME"\""
+        find $fdirname -type f -name "*-split-*" | sort | while read f; do
+          fn=$(basename $f)
+          echo "     curl -i -sX POST 'http://${INFLUXDB_HOST}:${INFLUXDB_PORT}/write?db="$INFLUXDB_NAME"' --data-binary @${WORKDIR}/$fn"
+        done
+      else
+        echo
+        echo "     Saved InfluxDB formatted data files ($counter lines) at:"
+        echo "     ${WORKDIR}/$fbasename ($counter)"
+        echo
+        echo "     InfluxDB import queries"
+        echo
+        echo "     curl -i -sX POST http://${INFLUXDB_HOST}:${INFLUXDB_PORT}/query --data-urlencode \"q=CREATE DATABASE "$INFLUXDB_NAME"\""
+        echo "     curl -i -sX POST 'http://${INFLUXDB_HOST}:${INFLUXDB_PORT}/write?db="$INFLUXDB_NAME"' --data-binary @"${WORKDIR}/$c""
+
+      fi
+    done
+  fi
+}
+
+help() {
+  echo
+  echo "Usage:"
+  echo
+  echo "$0 convert /path/to/summary-raw-scenarios-multi.gz"
+}
+
+case "$1" in
+  convert )
+    if [ -f "$2" ]; then
+      convert_to_influx influx "$2"
+    else
+      echo "error: $2 not does not exist"
+    fi
+    ;;
+  * )
+    help
+    ;;
+esac
