1faf17f32409ee0ebd99a1c11d7ee6b91d15d385
George Liu (eva2000) <eva2000@centminmod.com>
2024-02-29 20:49:53 +1000

    update wp login tests

--- tests/wordpress/wp-user-logins/wp-user-logins.js
+++ tests/wordpress/wp-user-logins/wp-user-logins.js
@@ -1,20 +1,41 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/centminmod/k6-reporter/cmm/dist/bundle.js";
+import {
+  jUnit,
+  textSummary,
+} from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 
 export let options = {
     stages: [
-        { duration: '1m', target: 10 }, // Ramp up to 10 users over 1 minute
-        { duration: '2m', target: 10 }, // Stay at 10 users for 2 minutes
-        { duration: '1m', target: 0 },  // Ramp down to 0 users over 1 minute
+        { duration: '45s', target: 10 }, // Ramp up to 10 users
+        { duration: '60', target: 10 }, // Stay at 10 users
+        { duration: '15s', target: 0 },  // Ramp down to 0 users
     ],
+    discardResponseBodies: true,
+    noConnectionReuse: false,
+    userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.66 Safari/537.36 OPR/89.0.4447.38",
+    summaryTrendStats: ["avg", "min", "med", "max", "p(95)", "p(99)", "p(99.99)", "count"],
+    tlsVersion: { min: "tls1.2", max: "tls1.3" },
+    // thresholds: {
+    //     'http_req_duration{gzip:yes}': ["avg<150", "p(95)<500"],
+    // },
 };
 
 const BASE_URL = __ENV.SITE_URL || 'http://localhost'; // Default to localhost if not provided
 
+export function handleSummary(data) {
+  return {
+    "result-scenarios.html": htmlReport(data),
+    stdout: textSummary(data, { indent: " ", enableColors: true }),
+    "summary-scenarios-htmlreport.json": JSON.stringify(data),
+  };
+}
+
 export default function () {
     // Dynamically construct the username based on the iteration (VU number)
     const username = `test${__VU}`;
-    const password = '3405691582'; // Assuming all users have the same password
+    const password = '3405691999'; // Assuming all users have the same password
 
     var payload = {
         log: username,
@@ -26,15 +47,20 @@ export default function () {
 
     var params = {
         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
+            "Content-Type": 'application/x-www-form-urlencoded',
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "accept-encoding": "gzip, deflate",
+            "accept-language": "en-US,en;q=0.9",
+            connection: "keep-alive",
         },
     };
 
     let response = http.post(`${BASE_URL}/wp-login.php`, payload, params);
+    // let response = http.post(`${BASE_URL}/wp-login.php`, payload, params, {insecureSkipTLSVerify: true});
 
     check(response, {
         'is status 200': (r) => r.status === 200,
-        'is redirected to wp-admin': (r) => r.url.includes('/wp-admin'),
+        // 'is redirected to wp-admin': (r) => r.url.includes('/wp-admin'),
     });
 
     // Optionally, add sleep to simulate think time
